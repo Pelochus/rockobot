9099e2806849d18fadac6690d952bb86756fbbd0
Pelochus <49835792+Pelochus@users.noreply.github.com>
2023-05-12 19:28:16 +0200

    Updated Arduino file

--- arduino/rockobot-main/rockobot-main.ino
+++ arduino/rockobot-main/rockobot-main.ino
@@ -11,15 +11,26 @@
 
 #include <HCSR04.h> // RCWL-1601 is compatible
 
-#define LMOTOR_PIN 2
-#define RMOTOR_PIN 3
+// TODO change pin numbers
+#define LMOTOR_EN 2
+#define LMOTOR_IN1 3
+#define LMOTOR_IN2 4
+#define RMOTOR_IN1 5
+#define RMOTOR_IN2 6
+#define RMOTOR_EN 7
+
 #define TRIGGER 4
-#define ECHO 5
+#define ECHO_FRONT 5
+#define ECHO_BACK 5
+
+#define IR_READ_FRONT_PIN A2
+#define IR_READ_BACK_PIN A2
 
 #define MAX_DISTANCE 200 // Adapt to ring maximum distance (in cm)
 #define ACTION_DELAY 50 // Delay in ms between one loop and another
 
-UltraSonicDistanceSensor us_sensor(TRIGGER, ECHO, MAX_DISTANCE);
+UltraSonicDistanceSensor us_sensor_front(TRIGGER, ECHO_FRONT, MAX_DISTANCE);
+UltraSonicDistanceSensor us_sensor_back(TRIGGER, ECHO_BACK, MAX_DISTANCE);
 
 // Example scheme: 
 // First read values from sensor, then think how to act and end with actuators
@@ -36,8 +47,17 @@ void act() {
 }
 
 void setup() {
-  pinMode(LMOTOR_PIN, OUTPUT);
-  pinMode(RMOTOR_PIN, OUTPUT);
+  /*
+  pinMode(LMOTOR_IN1, OUTPUT);
+  pinMode(LMOTOR_IN2, OUTPUT);
+  pinMode(LMOTOR_EN, OUTPUT);
+  pinMode(RMOTOR_IN1, OUTPUT);
+  pinMode(RMOTOR_IN2, OUTPUT);
+  pinMode(RMOTOR_EN, OUTPUT);
+
+  digitalWrite(LMOTOR_IN1, HIGH);
+  digitalWrite(LMOTOR_IN2, LOW);
+  */
 
   Serial.begin(9600); // Remove in final versions
 }
