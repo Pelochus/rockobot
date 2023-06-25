08d50ece9eb671265d45f58d7f713b9edb8da38c
Pelochus <49835792+Pelochus@users.noreply.github.com>
2023-06-25 21:31:09 +0200

    Expected to be final version, added extra delays

--- arduino/rockobot-main/rockobot-main.ino
+++ arduino/rockobot-main/rockobot-main.ino
@@ -38,12 +38,13 @@ UltraSonicDistanceSensor us_back(TRIGGER, ECHO_BACK, MAX_DISTANCE);
 L298N_Rockobot driver(ENA, ENB, IN1, IN2, IN3, IN4);
 
 const uint16_t FULL_POWER_THRESHOLD = 8000; // After 8 seconds, always use full power
-const uint16_t IR_THRESHOLD = 350; // If higher than this, we are entering danger zone (out of ring)
+const uint16_t IR_THRESHOLD = 300; // If higher than this, we are entering danger zone (out of ring)
 const uint8_t US_NEARBY_ENEMY = 30; // When is considered to be near an enemy (cm)
 const uint8_t FULL_POWER_NEAR_ENEMY = 10; // When under this value in cm, activate maximum speed
 const uint8_t STUCK_COUNT = 50; // If we reach 50 loops detecting black lines, we are stuck
-const uint8_t TURN_CYCLES = 14; // Loops (ACTION_DELAY = 50 ms) required for 90º rotation at 100% speed (We calculated 700 ms 90 deg at 16.7V battery)
+const uint8_t TURN_CYCLES = 700 / ACTION_DELAY; // Loops required for 90º rotation at 100% speed (We calculated 700 ms 90 deg at 16.7V battery)
 
+uint16_t extra_delay = 0;
 uint16_t initial_time = 0;
 uint16_t current_time = 0;
 uint8_t front_counter = 0;
@@ -53,30 +54,30 @@ uint8_t search_stage = 0;
 // Structure: 
 // First read values from sensor, then think what to do and send info to motors
 void rockobot_think() {
+  extra_delay = 800; // Add an extra delay when encountering black lines
 
-  //////////////////////////////////
+  ////////////////
   // Read Sensors
-  //////////////////////////////////
+  ////////////////
   uint8_t front_distance = us_front.measureDistanceCm();
   uint8_t back_distance = us_back.measureDistanceCm();
-  bool ir_front = analogRead(IR_FRONT) > IR_THRESHOLD;
-  bool ir_back = analogRead(IR_BACK) > IR_THRESHOLD;
-  bool ir_right = analogRead(IR_RIGHT) > IR_THRESHOLD;
-  bool ir_left = analogRead(IR_LEFT) > IR_THRESHOLD;
+  bool ir_front = (analogRead(IR_FRONT) > IR_THRESHOLD);
+  bool ir_back = (analogRead(IR_BACK) > IR_THRESHOLD);
+  bool ir_right = (analogRead(IR_RIGHT) > IR_THRESHOLD);
+  bool ir_left = (analogRead(IR_LEFT) > IR_THRESHOLD);
 
-  //////////////////////////////////
+  /////////
   // Think
-  //////////////////////////////////
-
+  /////////
   uint8_t speed = 100;
-  direction_t direction = FAST_STOP;
+  direction_t direction = driver.get_direction(); // Maintain same path while thinking
   
   // Anti-ramp
   if ((ir_front ? 1 : 0) + (ir_back ? 1 : 0) + 
       (ir_right ? 1 : 0) + (ir_left ? 1 : 0) > 2) {
     direction = RIGHT;
   }
-  else if (ir_front > IR_THRESHOLD) {
+  else if (ir_front) {
     direction = BACKWARD;
 
     if (front_counter > STUCK_COUNT) {
@@ -87,7 +88,7 @@ void rockobot_think() {
     front_counter++;
     back_counter = 0;
   }
-  else if (ir_back > IR_THRESHOLD) {
+  else if (ir_back) {
     direction = FORWARD;
 
     if (back_counter > STUCK_COUNT) {
@@ -98,21 +99,25 @@ void rockobot_think() {
     front_counter = 0;
     back_counter++;
   }
-  else if (ir_right > IR_THRESHOLD) {
+  // We want even higher delays on rotations, because robot is slower with only one motor
+  else if (ir_right) {
+    extra_delay += 200;
     direction = WIDE_LEFT;
   }
-  else if (ir_left > IR_THRESHOLD) {
+  else if (ir_left) {
+    extra_delay += 200;
     direction = WIDE_RIGHT;
   }
   else {
     ///////////////////////////////////////////////////////
     // Not in danger zone, search and target enemy with US
     ///////////////////////////////////////////////////////
-
+    extra_delay = 0; // Reset delay if not in danger (black lines)
+    direction = FORWARD;
     speed = 85;
     
     uint8_t us_min = (front_distance > back_distance) ? back_distance : front_distance;
-    bool search = (us_min < US_NEARBY_ENEMY) ? false : true; // Decide whether to skip or not search stage
+    bool search = us_min > US_NEARBY_ENEMY; // Decide whether to skip or not search stage
 
     //////////
     // Search
@@ -145,9 +150,9 @@ void rockobot_think() {
     if (us_min < FULL_POWER_NEAR_ENEMY) speed = 100;
   }
 
-  ////////////////
-  // Finished thinking
-  ////////////////
+  //////////////////////
+  // Act after thinking
+  //////////////////////
 
   driver.set_speed_percentage(speed);
   driver.set_direction(direction);
@@ -159,8 +164,6 @@ void setup() {
   pinMode(IR_BACK, INPUT);
   pinMode(IR_RIGHT, INPUT);
   pinMode(IR_LEFT, INPUT);
-
-  Serial.begin(9600);
 
   driver.set_speed_percentage(0);
   delay(STARTUP_DELAY); // Mandatory delay for official competition
@@ -178,5 +181,5 @@ void loop() {
     driver.set_speed_percentage(100);
   }
 
-  delay(ACTION_DELAY);
+  delay(ACTION_DELAY + extra_delay);
 }
